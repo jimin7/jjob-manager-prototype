4ae21574a0300d96d2e44742a55277fc1af020c5
Jimin Kim <spring7day@gmail.com>
2020-07-04 00:48:05 +0900

    <Navbar/>로 변경

--- src/components/WorkbenchTop.js
+++ src/components/WorkbenchTop.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { Button, Classes, Icon } from "@blueprintjs/core";
+import {Button, Classes, Icon, Navbar, NavbarGroup, NavbarHeading, Alignment, NavbarDivider} from "@blueprintjs/core";
 
 function WorkbenchTop({ loginUser, login, logout }) {
   return (
-    <>
-      <div style={{ height: "50px", width: "100%", border: "solid" }}>
-        {loginUser === null ? (
-          <Button
-            className={Classes.DARK}
-            icon={"login"}
-            onClick={() => login("jimin")}
-          >
-            로그인
-          </Button>
-        ) : (
-          <Button className={Classes.DARK} icon={"logout"} onClick={logout}>
-            로그아웃
-          </Button>
-        )}
-      </div>
-    </>
+      <>
+          <Navbar>
+              <NavbarGroup align={Alignment.LEFT}>
+                  <NavbarHeading>J-Jobs</NavbarHeading>
+              </NavbarGroup>
+              <NavbarGroup align={Alignment.RIGHT}>
+                  {loginUser === null ? (
+                      <Button
+                          className={Classes.MINIMAL}
+                          icon={"log-in"}
+                          onClick={() => login("jimin")}
+                      />
+                  ) : (
+                      <Button className={Classes.MINIMAL} icon={"log-out"} onClick={logout}/>
+                  )}
+              </NavbarGroup>
+          </Navbar>
+      </>
   );
 }
 
